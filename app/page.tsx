733904c42f9a8defc38fefeb0a0b6d3e8f9bcd81
obrkn <k33mdukb9n@yahoo.co.jp>
2023-11-02 22:43:39 -0700

    add react-modal

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Highlighter from "react-highlight-words";
 import React, { useEffect } from "react";
 import DefaultData from "@/app/data";
 import Image from "next/image";
+import Modal from "react-modal";
 
 export interface ListItem {
   id: string;
@@ -31,6 +32,10 @@ enum Color {
   Blue700,
   Amber700,
   Pink700,
+  Purple600,
+  Cyan600,
+  Green800,
+  Red500,
 }
 enum Icon {
   Dragon,
@@ -42,7 +47,7 @@ const Themes = [
     id: "ameblo#tohokulax08",
     title: "東北大学男子ラクロス部",
     description: "～東北大学男子ラクロス部員のブログ～",
-    period: "2008年 - 現在",
+    period: "2008年 - 更新中",
     color: Color.Violet900,
     icon: Icon.Dragon,
   },
@@ -50,7 +55,7 @@ const Themes = [
     id: "ameblo#humblers",
     title: "HUMBLERSのブログ",
     description: "～ハンブロ～",
-    period: "2010年 - 現在",
+    period: "2010年 - 更新中",
     color: Color.Blue700,
     icon: Icon.Humbler,
   },
@@ -59,7 +64,7 @@ const Themes = [
     title: "東北大学男子ラクロス部チームスタッフブログ",
     description: "東北大学男子ラクロス部チームスタッフのブログです。",
     period: "2010年 - 2019年",
-    color: Color.Amber700,
+    color: Color.Purple600,
     icon: Icon.Dragon,
   },
   {
@@ -70,9 +75,42 @@ const Themes = [
     color: Color.Pink700,
     icon: Icon.Humbler,
   },
+  {
+    id: "binwan-tohoku#jugem",
+    title: "ウィンブレではなく、ラクジャン",
+    description:
+      "更新数のやばい、東北大学学友会男子ラクロス部マネージャーのブログ",
+    period: "2008年 - 2010年",
+    color: Color.Amber700,
+    icon: Icon.Dragon,
+  },
+  {
+    id: "ameblo#jolax-tohoku",
+    title: "まめおかき",
+    description: "～東北大女子ラクロス部の成長の記録～",
+    period: "2009年 - 2010年",
+    color: Color.Cyan600,
+    icon: Icon.Humbler,
+  },
+  {
+    id: "ameblo#passer-hiro53",
+    title: "ケニアより愛を込めて",
+    description: "日本が恋しいです。冗談だけど。",
+    period: "2007年 - 2011年",
+    color: Color.Green800,
+    icon: Icon.Dragon,
+  },
+  {
+    id: "ameblo#tohokulaxmgs",
+    title: "ウィンブレではなく、ラクジャンのポケット",
+    description: "ジュゲムの限界にアメブロが挑みます。",
+    period: "2009年",
+    color: Color.Red500,
+    icon: Icon.Dragon,
+  },
 ];
 
-export default function Sub() {
+export default function Home() {
   const [query, setQuery] = React.useState("");
   const [rawData, setRawData] = React.useState<ListItem[]>(DefaultData);
   const [results, setResults] = React.useState<ListItem[]>(DefaultData);
@@ -88,6 +126,10 @@ export default function Sub() {
     }
     return Themes[0];
   });
+
+  React.useEffect(() => {
+    Modal.setAppElement("#__next");
+  }, []);
 
   React.useEffect(() => {
     setRawData(
@@ -126,7 +168,7 @@ export default function Sub() {
 
   React.useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 1000) {
+      if (window.scrollY > 600) {
         setIsLogo(true);
       } else {
         setIsLogo(false);
@@ -148,8 +190,16 @@ export default function Sub() {
             : currentTheme.color === 1
             ? "border-blue-700"
             : currentTheme.color === 2
-            ? `border-amber-700`
-            : `border-pink-700`
+            ? "border-amber-700"
+            : currentTheme.color === 3
+            ? `border-pink-700`
+            : currentTheme.color === 4
+            ? `border-purple-600`
+            : currentTheme.color === 5
+            ? `border-cyan-600`
+            : currentTheme.color === 6
+            ? `border-green-800`
+            : `border-red-500`
         }`}
       >
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -170,7 +220,7 @@ export default function Sub() {
           </svg>
         </div>
         <input
-          className="appearance-none bg-transparent border-none flex-grow text-gray-700 mr-3 py-1 pl-10 leading-tight focus:outline-none"
+          className="appearance-none bg-transparent border-none grow text-gray-700 mr-3 py-1 pl-10 leading-tight focus:outline-none"
           type="text"
           value={query}
           onChange={handleSearch}
@@ -206,7 +256,15 @@ export default function Sub() {
                 ? "hover:border-blue-700"
                 : currentTheme.color === 2
                 ? "hover:border-amber-700"
-                : "hover:border-pink-700"
+                : currentTheme.color === 3
+                ? "hover:border-pink-700"
+                : currentTheme.color === 4
+                ? "hover:border-purple-600"
+                : currentTheme.color === 5
+                ? "hover:border-cyan-600"
+                : currentTheme.color === 6
+                ? "hover:border-green-800"
+                : "hover:border-red-500"
             }`}
           >
             <a
@@ -223,7 +281,15 @@ export default function Sub() {
                     ? "text-blue-700"
                     : currentTheme.color === 2
                     ? "text-amber-700"
-                    : "text-pink-700"
+                    : currentTheme.color === 3
+                    ? "text-pink-700"
+                    : currentTheme.color === 4
+                    ? "text-purple-600"
+                    : currentTheme.color === 5
+                    ? "text-cyan-600"
+                    : currentTheme.color === 6
+                    ? "text-green-800"
+                    : "text-red-500"
                 }`}
                 highlightClassName="bg-yellow-200"
                 searchWords={[query]}
@@ -262,87 +328,95 @@ export default function Sub() {
           </div>
         ))}
       </ul>
-      {isOpen && (
-        <div
-          onClick={(e) => {
-            if (e.target !== e.currentTarget) return;
-            setIsOpen(false);
-          }}
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-        >
-          <div className="bg-white p-4 rounded">
-            <div className="text-right">
-              <button
-                onClick={() => setIsOpen(false)}
-                className="text-gray-500 hover:text-gray-600"
-              >
-                <svg
-                  className="w-6 h-6 fill-current"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="m10.707 10 4.147-4.146a.5.5 0 1 0 -.707-.708l-4.147 4.146-4.146-4.146a.5.5 0 0 0 -.708.708l4.146 4.146-4.146 4.147a.5.5 0 1 0 .708.707l4.146-4.146 4.147 4.146a.5.5 0 1 0 .707-.707z" />
-                </svg>
-              </button>
-            </div>
-            <ul className="grid w-full gap-6 md:grid-cols-3 my-5">
-              {Themes.map((item) => (
-                <li key={item.id} className="relative">
-                  <p className="absolute bottom-1 right-2 text-xs text-gray-400">
-                    {item.id}
-                  </p>
-                  <input
-                    type="radio"
-                    id={item.id}
-                    name="blogTag"
-                    defaultChecked={currentTheme.id === item.id}
-                    className="hidden peer"
-                    onChange={(e) => {
-                      if (e.target.checked) {
-                        setCurrentTag(item.id);
-                        setIsOpen(false);
-                      }
-                    }}
-                  />
-                  <label
-                    htmlFor={item.id}
-                    className={`inline-flex w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer  hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50 ${
-                      item.color === 0
-                        ? "peer-checked:border-violet-900"
-                        : item.color === 1
-                        ? "peer-checked:border-blue-700"
-                        : item.color === 2
-                        ? "peer-checked:border-amber-700"
-                        : "peer-checked:border-pink-700"
-                    }`}
-                  >
-                    <div className="text-center w-full">
-                      <p className="text-bold text-xs md:text-base">
-                        {item.title}
-                      </p>
-                      <p className="text-xs md:text-sm">{item.description}</p>
-                      <p className="text-xs">{item.period}</p>
-                    </div>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center overflow-y-auto"
+        className="bg-white p-4 m-4 rounded outline-none max-h-full overflow-y-auto"
+      >
+        <div className="text-right">
+          <button
+            onClick={() => setIsOpen(false)}
+            className="text-gray-500 hover:text-gray-600"
+          >
+            <svg
+              className="w-6 h-6 fill-current"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="m10.707 10 4.147-4.146a.5.5 0 1 0 -.707-.708l-4.147 4.146-4.146-4.146a.5.5 0 0 0 -.708.708l4.146 4.146-4.146 4.147a.5.5 0 1 0 .708.707l4.146-4.146 4.147 4.146a.5.5 0 1 0 .707-.707z" />
+            </svg>
+          </button>
         </div>
-      )}
+        <ul className="grid w-full gap-3 md:grid-cols-3 mt-5">
+          {Themes.map((item) => (
+            <li key={item.id} className="relative h-auto">
+              <p className="absolute bottom-1 right-2 text-xs text-gray-400">
+                {item.id}
+              </p>
+              <input
+                type="radio"
+                id={item.id}
+                name="blogTag"
+                defaultChecked={currentTheme.id === item.id}
+                className="hidden peer"
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setCurrentTag(item.id);
+                  }
+                }}
+              />
+              <label
+                htmlFor={item.id}
+                className={`inline-flex w-full h-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer  hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50 ${
+                  item.color === 0
+                    ? "peer-checked:border-violet-900"
+                    : item.color === 1
+                    ? "peer-checked:border-blue-700"
+                    : item.color === 2
+                    ? "peer-checked:border-amber-700"
+                    : item.color === 3
+                    ? "peer-checked:border-pink-700"
+                    : item.color === 4
+                    ? "peer-checked:border-purple-600"
+                    : item.color === 5
+                    ? "peer-checked:border-cyan-600"
+                    : item.color === 6
+                    ? "peer-checked:border-green-800"
+                    : "peer-checked:border-red-500"
+                }`}
+              >
+                <div className="text-center w-full">
+                  <p className="text-bold text-xs md:text-base">{item.title}</p>
+                  <p className="text-xs md:text-sm">{item.description}</p>
+                  <p className="text-xs">{item.period}</p>
+                </div>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </Modal>
       <button
         onClick={() => {
           window.scrollTo({ top: 0, behavior: "smooth" });
         }}
-        className={`fixed flex justify-center items-center bottom-2 right-2 w-16 h-16 rounded-full shadow-lg hover:-rotate-12 hover:scale-150 hover:-translate-x-6 hover:-translate-y-6 ${
+        className={`fixed flex justify-center items-center bottom-2 right-4 w-16 h-16 rounded-full shadow-lg hover:-rotate-12 hover:scale-150 hover:-translate-x-6 hover:-translate-y-6 ${
           currentTheme.color === 0
             ? "bg-violet-900"
             : currentTheme.color === 1
             ? "bg-blue-700"
             : currentTheme.color === 2
             ? "bg-amber-700"
-            : "bg-pink-700"
+            : currentTheme.color === 3
+            ? "bg-pink-700"
+            : currentTheme.color === 4
+            ? "bg-purple-600"
+            : currentTheme.color === 5
+            ? "bg-cyan-600"
+            : currentTheme.color === 6
+            ? "bg-green-800"
+            : "bg-red-500"
         } ${
           isLogo ? "opacity-100" : "opacity-0 pointer-events-none"
         } transition-opacity duration-500`}
